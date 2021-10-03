f46241f8ad7a46415139b3ca7179fd6d9ec369c6
Brian <briwag88@hotmail.com>
2021-10-03 16:04:15 -0600

    adding admin_proyections

---
+++ REST_API_SERVER/Controllers/Admin_projection.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore.Query;
+using System.Threading.Tasks;
+using REST_API_SERVER.Database_Models;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+using Npgsql;
+using NpgsqlTypes;
+using Npgsql.Util;
+using Npgsql.Logging;
+using Npgsql.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace REST_API_SERVER.Controllers
+{
+    public class Admin_projection : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
