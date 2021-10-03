a9696f6b860dcd4b98b5e06699db46e820bda3e9
Adrián <adriantec2019@gmail.com>
2021-10-03 15:26:23 -0600

    fix

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -4,31 +4,39 @@ import { AdminComponent } from './admin/admin/admin.component';
 import { MovieMenuComponent } from './movie-menu/movie-menu.component';
 import { BillboardComponent } from './billboard/billboard.component';
 import { MovieTheatersBoardsComponent } from './movie-theaters-boards/movie-theaters-boards.component';
+import { PagesComponent } from './pages.component';
 
 const routes: Routes = [
   {
-    path: 'movieMenu',
-    component: MovieMenuComponent,
-  },
-  {
-    path: 'admin',
-    component: AdminComponent,
-  },
-  {
-    path: 'billboard',
-    component: BillboardComponent,
-  },
-  {
-    path: 'movieTheatreSelection',
-    component: MovieTheatersBoardsComponent,
-  },
-  {
-    path: '**',
-    redirectTo: 'movieTheatreSelection',
-  },
-  {
     path: '',
-    redirectTo: 'movieTheatreSelection',
+    component: PagesComponent,
+    //canActivate:true,
+    children: [
+      {
+        path: 'movieMenu',
+        component: MovieMenuComponent,
+      },
+      {
+        path: 'admin',
+        component: AdminComponent,
+      },
+      {
+        path: 'billboard',
+        component: BillboardComponent,
+      },
+      {
+        path: 'movieTheatreSelection',
+        component: MovieTheatersBoardsComponent,
+      },
+      {
+        path: '**',
+        redirectTo: 'movieTheatreSelection',
+      },
+      {
+        path: '',
+        redirectTo: 'movieTheatreSelection',
+      },
+    ],
   },
 ];
 
