1f95e873ace7e534d60a0e6b101d95359fa5a553
Brian <briwag88@hotmail.com>
2021-10-03 21:33:08 -0600

    Admin Movie get

---
+++ REST_API_SERVER/Controllers/MoviesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore.Query;
+using System.Threading.Tasks;
+using REST_API_SERVER.Database_Models;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+using Npgsql;
+using NpgsqlTypes;
+using Npgsql.Util;
+using Npgsql.Logging;
+using Npgsql.Schema;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+
+namespace REST_API_SERVER.Controllers
+{
+    [ApiController]
+    [Route("Admin/Movies")]
+    public class MoviesController : Controller
+    {
+        CineTEC_Context Db = new CineTEC_Context();
+        [HttpGet]
+        public List<Movie> Get()
+        {
+            var movies = Db.Movies.Include(m=> m.Projections).ToList();
+            return movies;
+        }
+    }
+}
